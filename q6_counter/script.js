a9ba40e9b6878484f14db1d9d4a616617307389e
Eric Er Chin Ann <er_chin_ann@hotmail.com>
2022-10-07 07:56:18 +0000

    WIP

--- q6_counter/script.js
+++ q6_counter/script.js
@@ -1,5 +1,5 @@
 let btnAdd = document.querySelector(`#increment`).addEventListener(`click`, function(){
-    let boxValueUp = document.querySelector(`#box`).innerHTML += 1;
+    let boxValueUp = document.querySelector(`#box`).innerHTML -=  -1;
 });
 
 let btnMinus = document.querySelector(`#decrement`).addEventListener(`click`, function(){
